ae505ba1ee9cd87e7e529a525310ed469a545ba7
nguyendangdinh47 <nguyendangdinh47@gmail.com>
2019-11-12 20:39:31 +0700

    no message

---
+++ Web/js.js
@@ -0,0 +1,11 @@
+// hàm đóng mở menu
+function myFunction() {
+          var x = document.getElementById("topnav");
+          if (x.style.display == "flex") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "flex";
+          }
+        }     
+
+      
---
+++ reponsive/reponsive.js
@@ -0,0 +1,9 @@
+function openNav(id, elemnt){
+
+        document.getElementById(""+id).style.display = "block";
+        elemnt.style.display = "none"
+    
+}
+function closeNav(id){
+        document.getElementById(""+id).style.display = "none";
+}
